bd0efa034d53c0d34768edb444492b3d23e99590
Chuck <cezimmerman90@gmail.com>
2023-10-18 21:56:29 -0400

    1.0.0 build milestone

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,177 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import Modal from "../components/Modal";
 
 export default function Home() {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+
+  const projects = [
+    {
+      name: "Coupon Clipping App",
+      content:
+        "ReactJS application housed in a WordPress Plugin. Automated code deployments with Bitbucket Pipelines. NodeJS/ExpressJS back-end hosted in containerized Azure App Service. I personally architected the solution end to end and wrote the spec for all involved APIs and their integrations to services like Mulesoft and Inmar.",
+      imageSrc: "/images/react-wordpress.png",
+      uri: "https://shopfamilyfare.com/coupons",
+    },
+    {
+      name: "Loyalty Points App",
+      content:
+        "ReactJS application housed in a WordPress Plugin. Automated code deployments with Bitbucket Pipelines. NodeJS/ExpressJS back-end hosted in containerized Azure App Service. I personally architected the solution end to end and wrote the spec for all involved APIs and their integrations to services like Mulesoft and Inmar.",
+      imageSrc: "/images/rewards-points.png",
+      uri: "https://shopfamilyfare.com/rewards",
+    },
+    {
+      name: "Grocery Shopping iOS/Android App",
+      content: "React Native / Redux app for six brick and mortar brands - Family Fare, D&W Fresh Market, Family Fresh Market, Dan's Supermarkets, VG's Grocery, and Forest Hills Foods. Hundreds of thousands of installs. Create and manage an account, view the weekly ad, find a store near you, clip coupons, earn points and rewards, contact support, track your order by fine GPS location for instant grocery pickups.",
+      imageSrc: "/images/grocery-app.jpg",
+      uri: "https://www.shopfamilyfare.com/download-mobile-app",
+    },
+    {
+      name: "Apple Wallet Card Generator",
+      content: "A Node/Express application to generate Apple Wallet loyalty cards so that our customers can scan at checkout.",
+      imageSrc: "/images/applewallet.jpg",
+      uri: "https://www.shopfamilyfare.com/download-mobile-app",
+    },
+    {
+      name: "Self-editing chatbot",
+      content: "Using OpenAI's GPT API and prompt engineering, a node application that ... built itself! Using a chat interface, I can send instructions to a chatbot to edit the application itself. The user is prompted in the terminal to accept the changes.",
+      imageSrc: "/images/self-editing-chatbot.jpg",
+      uri: "https://github.com/chuckzee/openai-selfedit",
+    },
+    {
+      name: "AI Dungeon Master Chatbot",
+      content: "An OpenAI GPT dungeon master with a Discord bot integration. 'dungeonmAIster' (I'm bad at naming things) can /start a D&D session, and then responds to users' /action commands describing what they do. The bot generates a dungeon in JSON format and records the status of rooms and exploration.",
+      imageSrc: "/images/dungeonmAIster.jpg",
+      uri: "https://github.com/chuckzee/dungeonmAIster",
+    },
+    {
+      name: "monsterdle - wordle for D&D",
+      content: "Caught up in the wordle craze, I built a Dart/Flutter app with a Node/Express back-end that lets users guess the 'monster of the day' from fifth edition Dungeons & Dragons. I had never used Flutter before, so it was a learning experience.",
+      imageSrc: "/images/monsterdle.jpg",
+      uri: "https://github.com/chuckzee/monsterdle",
+    },
+    {
+      name: "Gordon Now Delivery Tracking App",
+      content: "My first React Native mobile app. In Gordon Food Service\'s B2B business, truck deliveries can sometimes happen every day or multiple times a week - kitchen managers could track their order and how many stops away their order is at a given moment. They were also able to preview what was on the truck - were there are necessary replacements? This information was the lifeblood of restaurants keeping operations going smoothly. This app was supported by a Kotlin back-end.",
+      imageSrc: "/images/gordonnowapp.jpg",
+      uri: "",
+    },
+    {
+      name: "Gordon Now Marketing Site",
+      content: "A marketing site & editing tool for customers using the Gordon Now tool. It included an app that let users edit common products they would order so that their Gordon Now Voice agent (we used Google Homes and Google Minis) would recognize them - aka 'aliases.' ReactJS and PHP primarily.",
+      imageSrc: "/images/gordonnowcom.png",
+      uri: "",
+    },
+    {
+      name: "A candy store website?",
+      content: "One of the few Magento 2 products I've done - back in 2015 no less - this site is still kicking! I don't have a ton to say about it, I mostly just really enjoy the goofy CSS animations and things I did for it.",
+      imageSrc: "/images/shopthebulkstore.png",
+      uri: "https://www.shopthebulkstore.com/",
+    },
+    {
+      name: "This website",
+      content: "This website is a weird vaporwave ... piece of paper? Built with NextJS and deployed into Azure.",
+      imageSrc: "/images/chuckzee.png",
+      uri: "https://chuckzee.com",
+    },
+  ];
+
+  const openModal = (project) => {
+    setModalContent(project);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => setModalOpen(false);
+
+  const getRandomValue = () => {
+    return Math.floor(Math.random() * 8) + 7; // generates a number between 7 and 14
+  };
+
+  const renderProjectSection = (projectSubset) => (
+    <div className="projects-list">
+      {projectSubset.map((project, index) => (
+        <a
+          className="project-link"
+          key={index}
+          onClick={() => openModal(project)}
+          style={{ backgroundImage: `url(${project.imageSrc})` }}
+          data-color-value={getRandomValue()}
+        >
+          {project.name}
+        </a>
+      ))}
+    </div>
+  );
+
+  const chunksOfProjects = [];
+  for (let i = 0; i < projects.length; i += 6) {
+    chunksOfProjects.push(projects.slice(i, i + 6));
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      {chunksOfProjects.map((chunk, idx) => (
+        <div class="paper-container" key={idx}>
+          <div className="paper">
+            <div className="title-container">
+              <h1 className="page-title">Chuck Zimmerman</h1>
+              <span>
+                Engineering Manager, Senior Software Developer, Solutions
+                Architect
+              </span>
+            </div>
+            <div className="contact-section">
+              <div className="contact-list">
+                <a href="mailto:inquiries@chuckzee.com" target="_blank">Email</a>
+                <a href="https://github.com/chuckzee" target="_blank">
+                  GitHub
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/chuckzimmermanmi/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+            <div className="skills-section">
+              <h2 className="section-title">Skills</h2>
+              <ul className="skills-list">
+                <li>Javascript ES6</li>
+                <li>Typescript</li>
+                <li>ReactJS</li>
+                <li>React Native</li>
+                <li>NodeJS</li>
+                <li>ExpressJS</li>
+                <li>RESTful APIs</li>
+                <li>Python</li>
+                <li>PHP</li>
+                <li>Java</li>
+                <li>Kotlin</li>
+                <li>Docker</li>
+                <li>CI/CD</li>
+                <li>Jest</li>
+                <li>Flutter</li>
+                <li>Accessibility</li>
+                <li>Microsoft Azure</li>
+                <li>Google Cloud Platform</li>
+                <li>Solutions Architecture</li>
+                <li>Software Engineering Management</li>
+                <li>Agile Scrum Master</li>
+                <li>Bitbucket Pipelines</li>
+                <li>WordPress Plugin Development</li>
+                <li>HTML/CSS</li>
+              </ul>
+            </div>
+            <div className="projects-section">
+              <h2 className="section-title">Projects</h2>
+              {renderProjectSection(chunk)}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
+      <Modal isOpen={isModalOpen} onClose={closeModal} content={modalContent} />
     </main>
-  )
+  );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Modal({ isOpen, onClose, content }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="modal-content bg-white p-5 rounded shadow-lg w-3/4">
+        {content.imageSrc && (
+          <a target="_blank" href={content.uri} className="image-container relative">
+            <Image
+              src={content.imageSrc}
+              alt="Project Image Description" 
+              width={1920} 
+              height={1080}
+              className="modalImage"
+            />
+          </a>
+        )}{" "}
+        <p>{content.content}</p>
+        <button
+          onClick={onClose}
+          className="mt-4 px-4 py-2 bg-red-600 text-white rounded"
+        >
+          Close
+        </button>
+      </div>
+      <div
+        className="fixed inset-0 bg-black opacity-50"
+        onClick={onClose}
+      ></div>
+    </div>
+  );
+}
